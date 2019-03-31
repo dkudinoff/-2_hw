91191110499c81850480088276aced9082e8aff5
dkudinoff <daniil.kudinoff@gmail.com>
2019-04-01 02:13:44 +0300

    задания 1-2 для урока 1

---
+++ hw1_Kudinov_Daniil/Csharp2_hw_Kudinov_Daniil/Asteroid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game_Kudinov_Daniil
+
+{
+    class Asteroid : BaseObject
+    {
+        string path = ".\\img\\asteroid_01.png";
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Image asteroidImage = Image.FromFile(path);
+            Game.Buffer.Graphics.DrawImage(asteroidImage, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+    }
+}

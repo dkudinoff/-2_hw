ecfe3401fd449f5537a17100b31a0c088119e7e4
dkudinoff <daniil.kudinoff@gmail.com>
2019-04-01 02:19:11 +0300

    задания 1-2 для урока 1

---
+++ hw1_Kudinov_Daniil/Csharp2_hw_Kudinov_Daniil/Planet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Game_Kudinov_Daniil
+{
+    /// <summary>
+    /// Класс Планета. Использует свое изображение для отрисовки.
+    /// Использует свою перегрузку метода Update
+    /// </summary>
+    class Planet : BaseObject
+    {
+        string path = ".\\img\\planet_01.png";
+        Random rnd = new Random();
+        public Planet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Image asteroidImage = Image.FromFile(path);
+            Game.Buffer.Graphics.DrawImage(asteroidImage, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X > (Game.Width + Size.Width))
+            {
+                Pos.X = 0- Size.Width;
+                Pos.Y = rnd.Next(600)-150;
+            }
+        }
+    }
+}
+
